dadb125db2b84e5f22390f8e2ed2dcfdfe722dc0
JobGetabu <getabujob@gmail.com>
2018-07-24 15:47:06 +0300

    added app executors plus multidex class

---
+++ app/src/main/java/com/job/darasalecturer/DarasaLec.java
@@ -0,0 +1,46 @@
+package com.job.darasalecturer;
+
+import android.support.annotation.NonNull;
+import android.support.multidex.MultiDexApplication;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+/**
+ * Created by Job on Tuesday : 7/24/2018.
+ */
+public class DarasaLec extends MultiDexApplication {
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore mFirestore;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //firebase init
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    // do stuff
+                    // Check if user is signed in (non-null) and update UI accordingly.
+
+                    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                            .setPersistenceEnabled(true)
+                            .setTimestampsInSnapshotsEnabled(true)
+                            .build();
+                    mFirestore.setFirestoreSettings(settings);
+                }
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+}
