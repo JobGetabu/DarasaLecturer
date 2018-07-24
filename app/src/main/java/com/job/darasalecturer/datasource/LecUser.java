c8b80d57d23c9cd2c0cb13f2bf8cf16311fe9521
JobGetabu <getabujob@gmail.com>
2018-07-24 15:49:29 +0300

    added lec user pojo

---
+++ app/src/main/java/com/job/darasalecturer/datasource/LecUser.java
@@ -0,0 +1,56 @@
+package com.job.darasalecturer.datasource;
+
+import android.support.annotation.Keep;
+
+/**
+ * Created by Job on Tuesday : 7/24/2018.
+ */
+
+@Keep
+public class LecUser {
+    private String firstname;
+    private String lastname;
+    private String devicetoken;
+
+    public LecUser() {
+    }
+
+    public LecUser(String firstname, String lastname, String devicetoken) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.devicetoken = devicetoken;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDevicetoken() {
+        return devicetoken;
+    }
+
+    public void setDevicetoken(String devicetoken) {
+        this.devicetoken = devicetoken;
+    }
+
+    @Override
+    public String toString() {
+        return "LecUser{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", devicetoken='" + devicetoken + '\'' +
+                '}';
+    }
+}
