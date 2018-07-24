75db7241bf88b38a10b6a0206b220ad399f1db85
JobGetabu <getabujob@gmail.com>
2018-07-24 17:28:14 +0300

    login activity refactored to welcome activity

--- app/src/main/java/com/job/darasalecturer/ui/MainActivity.java
+++ app/src/main/java/com/job/darasalecturer/ui/MainActivity.java
@@ -1,15 +1,60 @@
 package com.job.darasalecturer.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.job.darasalecturer.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private FirebaseFirestore mFirestore;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // Sign in logic here.
+                    finish();
+                    sendToLogin();
+                }else {
+
+
+                    String userId = mAuth.getCurrentUser().getUid();
+
+                }
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/job/darasalecturer/ui/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package com.job.darasalecturer.ui;
+
+import android.os.Bundle;
+import android.support.design.button.MaterialButton;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.darasalecturer.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_positive)
+    MaterialButton loginPositive;
+    @BindView(R.id.login_negative)
+    MaterialButton loginNegative;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login_positive)
+    public void onLoginPositiveClicked() {
+
+    }
+
+    @OnClick(R.id.login_negative)
+    public void onLoginNegativeClicked() {
+        //TODO 1 : Take to browser to do lecturer account set up.
+    }
+}
