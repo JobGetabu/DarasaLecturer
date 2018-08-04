2e1349e32647f73575fc1f34657d2c525661692f
JobGetabu <getabujob@gmail.com>
2018-08-04 22:20:28 +0300

    downgraded app compact to alpha1 28

--- app/src/main/java/com/job/darasalecturer/ui/MainActivity.java
+++ app/src/main/java/com/job/darasalecturer/ui/MainActivity.java
@@ -3,14 +3,32 @@ package com.job.darasalecturer.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.job.darasalecturer.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_toolbar)
+    Toolbar mainToolbar;
+    @BindView(R.id.main_list)
+    RelativeLayout mainList;
+    @BindView(R.id.main_fab)
+    FloatingActionButton mainFab;
+
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
@@ -21,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mainToolbar);
 
         //firebase
         mAuth = FirebaseAuth.getInstance();
@@ -40,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                     // Sign in logic here.
                     finish();
                     sendToLogin();
-                }else {
+                } else {
 
 
                     String userId = mAuth.getCurrentUser().getUid();
@@ -56,5 +77,33 @@ public class MainActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(MainActivity.this, WelcomeActivity.class);
         startActivity(loginIntent);
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.hmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.hmenu_logout:
+                mAuth.signOut();
+
+                sendToLogin();
+                break;
+            case R.id.hmenu_settings:
+                Toast.makeText(this, "TODO -> settings", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
+    @OnClick(R.id.main_fab)
+    public void onFabClicked() {
     }
 }
