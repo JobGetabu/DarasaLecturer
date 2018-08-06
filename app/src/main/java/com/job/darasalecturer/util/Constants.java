a97e3957913f7a01eca4e05950173571e49bb4ef
JobGetabu <getabujob@gmail.com>
2018-08-06 14:23:37 +0300

    added git tracking files

---
+++ app/src/main/java/com/job/darasalecturer/util/Constants.java
@@ -0,0 +1,12 @@
+package com.job.darasalecturer.util;
+
+/**
+ * Created by Job on Friday : 8/3/2018.
+ */
+public class Constants {
+    public static final String LECUSERCOL = "LecUser";
+    public static final String LECTEACHCOL = "LecTeach";
+    public static final String LECTEACHTIMECOL = "LecTeachTime";
+    public static final String LECTEACHCOURSESUBCOL = "Courses";
+
+}
---
+++ app/src/main/java/com/job/darasalecturer/util/LessonViewHolder.java
@@ -0,0 +1,14 @@
+package com.job.darasalecturer.util;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Job on Monday : 8/6/2018.
+ */
+public class LessonViewHolder extends RecyclerView.ViewHolder {
+    public LessonViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
