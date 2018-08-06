820295aef3704040deac468d66cbefcce5dc673a
JobGetabu <getabujob@gmail.com>
2018-08-06 17:03:43 +0300

    added courses pojo and drawable helper

---
+++ app/src/main/java/com/job/darasalecturer/datasource/Course.java
@@ -0,0 +1,56 @@
+package com.job.darasalecturer.datasource;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Job on Monday : 8/6/2018.
+ */
+public class Course implements Parcelable {
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+    }
+
+    protected Course(Parcel in) {
+        this.name = in.readString();
+    }
+
+    public static final Parcelable.Creator<Course> CREATOR = new Parcelable.Creator<Course>() {
+        @Override
+        public Course createFromParcel(Parcel source) {
+            return new Course(source);
+        }
+
+        @Override
+        public Course[] newArray(int size) {
+            return new Course[size];
+        }
+    };
+}
--- app/src/main/java/com/job/darasalecturer/util/LessonViewHolder.java
+++ app/src/main/java/com/job/darasalecturer/util/LessonViewHolder.java
@@ -11,7 +11,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.job.darasalecturer.R;
 import com.job.darasalecturer.appexecutor.DefaultExecutorSupplier;
 import com.job.darasalecturer.datasource.LecTeachTime;
@@ -19,6 +22,9 @@ import com.job.darasalecturer.datasource.LecTeachTime;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.job.darasalecturer.util.Constants.LECTEACHCOL;
+import static com.job.darasalecturer.util.Constants.LECTEACHCOURSESUBCOL;
 
 /**
  * Created by Job on Monday : 8/6/2018.
@@ -111,6 +117,21 @@ public class LessonViewHolder extends RecyclerView.ViewHolder {
                     .tint()
                     .applyTo(lsLocImg);
         }
+    }
+
+    private void getCourses(LecTeachTime lecTeachTime){
+
+        mFirestore.collection(LECTEACHCOL).document(lecTeachTime.getLecteachid()).collection(LECTEACHCOURSESUBCOL)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> dbtask) {
+                        if (dbtask.isSuccessful()){
+
+                           //dbtask.getResult().toObjects()
+                        }
+                    }
+                });
     }
 
     private void addCourses(String course) {
