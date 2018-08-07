ccfb9a61f44d9d70a6a948d5342eba738562afb3
JobGetabu <getabujob@gmail.com>
2018-08-07 14:02:13 +0300

    timer complete : #bug 2 -> configuration change restarts timer

--- app/src/main/java/com/job/darasalecturer/ui/ScannerActivity.java
+++ app/src/main/java/com/job/darasalecturer/ui/ScannerActivity.java
@@ -1,13 +1,16 @@
 package com.job.darasalecturer.ui;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.job.darasalecturer.R;
+import com.victor.loading.newton.NewtonCradleLoading;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,6 +19,10 @@ public class ScannerActivity extends AppCompatActivity {
 
     @BindView(R.id.scan_toolbar)
     Toolbar scanToolbar;
+    @BindView(R.id.scan_loading)
+    NewtonCradleLoading scanLoading;
+    @BindView(R.id.scan_timer_text)
+    TextView scanTimerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,8 @@ public class ScannerActivity extends AppCompatActivity {
 
         setSupportActionBar(scanToolbar);
         getSupportActionBar().setTitle("");
+
+        initTimer();
     }
 
     @Override
@@ -47,5 +56,33 @@ public class ScannerActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void initTimer(){
+        scanLoading.start();
+        scanLoading.setLoadingColor(R.color.colorPrimary);
+
+        // 1min = 60 sec = 60000ms
+        new CountDownTimer(120000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                scanTimerText.setText(toMinutes(millisUntilFinished) +" : " + toSec(millisUntilFinished));
+            }
+
+            public void onFinish() {
+                scanTimerText.setText("done!");
+                scanLoading.stop();
+            }
+        }.start();
+    }
+
+    private String toMinutes(long millisUntilFinished){
+       long min =  (millisUntilFinished) / (1000 * 60);
+       return String.valueOf(min);
+    }
+
+    private String toSec(long millisUntilFinished){
+        long remainedSecs = millisUntilFinished / 1000;
+        return String.valueOf((remainedSecs % 60));
     }
 }
