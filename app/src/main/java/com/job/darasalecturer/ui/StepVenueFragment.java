b184d61e7a01ae5387616c24db288ac12de04b79
JobGetabu <getabujob@gmail.com>
2018-09-24 22:43:02 +0300

    injected step venue views

--- app/src/main/java/com/job/darasalecturer/ui/StepVenueFragment.java
+++ app/src/main/java/com/job/darasalecturer/ui/StepVenueFragment.java
@@ -2,18 +2,41 @@ package com.job.darasalecturer.ui;
 
 
 import android.os.Bundle;
+import android.support.design.button.MaterialButton;
+import android.support.design.chip.ChipGroup;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.job.darasalecturer.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StepVenueFragment extends Fragment {
 
+
+    @BindView(R.id.step_venue_name)
+    TextInputLayout stepVenueName;
+    @BindView(R.id.step_venue_department)
+    TextInputLayout stepVenueDepartment;
+    @BindView(R.id.step_venue_chipgroup)
+    ChipGroup stepVenueChipgroup;
+    @BindView(R.id.step_venue_course_btn)
+    MaterialButton stepVenueCourseBtn;
+    @BindView(R.id.step_venue_back)
+    TextView stepVenueBack;
+    @BindView(R.id.step_venue_next)
+    TextView stepVenueNext;
+    Unbinder unbinder;
 
     public StepVenueFragment() {
         // Required empty public constructor
@@ -24,7 +47,26 @@ public class StepVenueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_venue, container, false);
+        View view = inflater.inflate(R.layout.fragment_step_venue, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.step_venue_course_btn)
+    public void onStepVenueCourseBtnClicked() {
+    }
+
+    @OnClick(R.id.step_venue_back)
+    public void onStepVenueBackClicked() {
+    }
+
+    @OnClick(R.id.step_venue_next)
+    public void onStepVenueNextClicked() {
+    }
 }
