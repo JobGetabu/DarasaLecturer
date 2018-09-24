bcb7b57517f23449a506f158ee7a720db1126e6a
JobGetabu <getabujob@gmail.com>
2018-09-24 20:17:14 +0300

    added extra info fragment

---
+++ app/src/main/java/com/job/darasalecturer/ui/StepXinfoFragment.java
@@ -0,0 +1,30 @@
+package com.job.darasalecturer.ui;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.job.darasalecturer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StepXinfoFragment extends Fragment {
+
+
+    public StepXinfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_step_xinfo, container, false);
+    }
+
+}
