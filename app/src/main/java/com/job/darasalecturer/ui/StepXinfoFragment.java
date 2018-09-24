bd94c82f33379cc57249fd3d6e5b231975bd814a
JobGetabu <getabujob@gmail.com>
2018-09-24 22:44:30 +0300

    injected step x views

--- app/src/main/java/com/job/darasalecturer/ui/StepXinfoFragment.java
+++ app/src/main/java/com/job/darasalecturer/ui/StepXinfoFragment.java
@@ -2,18 +2,39 @@ package com.job.darasalecturer.ui;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.job.darasalecturer.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StepXinfoFragment extends Fragment {
 
+
+    @BindView(R.id.step_x_semester)
+    TextInputLayout stepXSemester;
+    @BindView(R.id.step_x_year)
+    TextInputLayout stepXYear;
+    @BindView(R.id.step_x_school)
+    TextInputLayout stepXSchool;
+    @BindView(R.id.step_x_acadyear)
+    TextInputLayout stepXAcadyear;
+    @BindView(R.id.step_x_back)
+    TextView stepXBack;
+    @BindView(R.id.step_x_finish)
+    TextView stepXFinish;
+    Unbinder unbinder;
 
     public StepXinfoFragment() {
         // Required empty public constructor
@@ -24,7 +45,22 @@ public class StepXinfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_xinfo, container, false);
+        View view = inflater.inflate(R.layout.fragment_step_xinfo, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.step_x_back)
+    public void onStepXBackClicked() {
+    }
+
+    @OnClick(R.id.step_x_finish)
+    public void onStepXFinishClicked() {
+    }
 }
