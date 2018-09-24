68e08c422e32fe2640e6bf5765cb6267c1b33b4f
JobGetabu <getabujob@gmail.com>
2018-09-24 22:19:28 +0300

    added add class view model

---
+++ app/src/main/java/com/job/darasalecturer/viewmodel/AddClassViewModel.java
@@ -0,0 +1,24 @@
+package com.job.darasalecturer.viewmodel;
+
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by Job on Monday : 9/24/2018.
+ */
+public class AddClassViewModel extends ViewModel {
+
+    private MediatorLiveData<Integer> currentStep;
+
+    public AddClassViewModel() {
+
+    }
+
+    public MediatorLiveData<Integer> getCurrentStep() {
+        return currentStep;
+    }
+
+    public void setCurrentStep(Integer currentStep) {
+        this.currentStep.setValue(currentStep);
+    }
+}
