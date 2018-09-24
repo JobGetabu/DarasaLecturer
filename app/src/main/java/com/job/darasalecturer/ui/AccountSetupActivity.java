d0d139ae29b9cd7dbe2d6beb4e83e86653afc898
JobGetabu <getabujob@gmail.com>
2018-09-24 12:15:56 +0300

    account setup activity

---
+++ app/src/main/java/com/job/darasalecturer/ui/AccountSetupActivity.java
@@ -0,0 +1,15 @@
+package com.job.darasalecturer.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.darasalecturer.R;
+
+public class AccountSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setup);
+    }
+}
