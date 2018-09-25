6b8b84c0fadc01b2ec456f449c80a717e0b14fa0
JobGetabu <getabujob@gmail.com>
2018-09-25 03:23:40 +0300

    injected account setup views

--- app/src/main/java/com/job/darasalecturer/ui/AccountSetupActivity.java
+++ app/src/main/java/com/job/darasalecturer/ui/AccountSetupActivity.java
@@ -1,15 +1,40 @@
 package com.job.darasalecturer.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.job.darasalecturer.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AccountSetupActivity extends AppCompatActivity {
+
+    @BindView(R.id.setup_toolbar)
+    Toolbar setupToolbar;
+    @BindView(R.id.setup_firstname)
+    TextInputLayout setupFirstname;
+    @BindView(R.id.setup_lastname)
+    TextInputLayout setupLastname;
+    @BindView(R.id.setup_school)
+    TextInputLayout setupSchool;
+    @BindView(R.id.setup_department)
+    TextInputLayout setupDepartment;
+    @BindView(R.id.setup_btn)
+    TextView setupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_setup);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.setup_btn)
+    public void onViewClicked() {
     }
 }
