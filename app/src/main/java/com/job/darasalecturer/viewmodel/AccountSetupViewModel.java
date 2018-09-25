1516b4ec69f446022bf280efb06989cb594ea2a9
JobGetabu <getabujob@gmail.com>
2018-09-25 21:42:40 +0300

    added efficient app executors. and account view model

--- app/src/main/java/com/job/darasalecturer/viewmodel/AccountSetupViewModel.java
+++ app/src/main/java/com/job/darasalecturer/viewmodel/AccountSetupViewModel.java
@@ -2,12 +2,22 @@ package com.job.darasalecturer.viewmodel;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.job.darasalecturer.appexecutor.DefaultExecutorSupplier;
+import com.job.darasalecturer.model.LecUser;
+import com.job.darasalecturer.repository.FirebaseDocumentLiveData;
+
+import static com.job.darasalecturer.util.Constants.LECUSERCOL;
 
 /**
  * Created by Job on Tuesday : 9/25/2018.
@@ -20,12 +30,64 @@ public class AccountSetupViewModel extends AndroidViewModel {
 
     public static final String TAG = "AccountSetupVM";
 
+    //few db references
+    private DocumentReference userRef;
+
+    //live datas
+    private FirebaseDocumentLiveData mUserLiveData;
+
+    //mediators
+    private MediatorLiveData<LecUser> lecUserMediatorLiveData = new MediatorLiveData<>();
+
     public AccountSetupViewModel(@NonNull Application application, FirebaseAuth mAuth, FirebaseFirestore mFirestore) {
         super(application);
         this.mAuth = mAuth;
         this.mFirestore = mFirestore;
+
+        //init db refs 
+        userRef = mFirestore.collection(LECUSERCOL).document(currentUserId);
+
+
+        //init livedatas
+        mUserLiveData = new FirebaseDocumentLiveData(userRef);
+
+        // Set up the MediatorLiveData to convert DataSnapshot objects into POJO objects
+        workOnUsersLiveData();
     }
 
+    private void workOnUsersLiveData() {
+
+        lecUserMediatorLiveData.addSource(mUserLiveData, new Observer<DocumentSnapshot>() {
+            @Override
+            public void onChanged(@Nullable final DocumentSnapshot documentSnapshot) {
+
+                if (documentSnapshot != null){
+
+                    DefaultExecutorSupplier.getInstance().forBackgroundTasks()
+                            .submit(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    lecUserMediatorLiveData.postValue(documentSnapshot.toObject(LecUser.class));
+
+                                }
+                            });
+
+                }else {
+                    lecUserMediatorLiveData.postValue(null);
+                }
+            }
+        });
+    }
+
+
+    public MediatorLiveData<LecUser> getLecUserMediatorLiveData() {
+        return lecUserMediatorLiveData;
+    }
+
+    public void setLecUserMediatorLiveData(LecUser lecUser) {
+        this.lecUserMediatorLiveData.setValue(lecUser);
+    }
 
     /**
      * Factory for instantiating the viewmodel
