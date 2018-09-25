36cd87e3659d0e66d74cdd7bfe8760be4a139454
JobGetabu <getabujob@gmail.com>
2018-09-25 21:22:04 +0300

    added account setup view model

---
+++ app/src/main/java/com/job/darasalecturer/viewmodel/AccountSetupViewModel.java
@@ -0,0 +1,9 @@
+package com.job.darasalecturer.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by Job on Tuesday : 9/25/2018.
+ */
+public class AccountSetupViewModel extends ViewModel {
+}
