9aa7c14b5ac7afe75796d1364bc60565d290f8de
JobGetabu <getabujob@gmail.com>
2018-09-25 03:30:53 +0300

    injected faq  views

--- app/src/main/java/com/job/darasalecturer/ui/FaqActivity.java
+++ app/src/main/java/com/job/darasalecturer/ui/FaqActivity.java
@@ -2,14 +2,28 @@ package com.job.darasalecturer.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import com.job.darasalecturer.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FaqActivity extends AppCompatActivity {
+
+    @BindView(R.id.faq_toolbar)
+    Toolbar faqToolbar;
+    @BindView(R.id.faq_progressBar)
+    ProgressBar faqProgressBar;
+    @BindView(R.id.faq_webView)
+    WebView faqWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        ButterKnife.bind(this);
     }
 }
