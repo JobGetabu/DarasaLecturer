d9c13bf9e2f99def7d9a80f05761f03c8a5d5b17
JobGetabu <getabujob@gmail.com>
2018-10-10 17:08:24 +0300

    added nearby test activity

--- app/src/main/java/com/job/darasalecturer/ui/MainActivity.java
+++ app/src/main/java/com/job/darasalecturer/ui/MainActivity.java
@@ -176,6 +176,10 @@ public class MainActivity extends AppCompatActivity {
 
                 sendToLogin();
                 break;
+
+            case R.id.hmenu_nearby:
+
+                startActivity(new Intent(this,NearbyActivity.class));
         }
 
         return true;
@@ -301,6 +305,7 @@ public class MainActivity extends AppCompatActivity {
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
                 // TODO Auto-generated method stub
+
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
