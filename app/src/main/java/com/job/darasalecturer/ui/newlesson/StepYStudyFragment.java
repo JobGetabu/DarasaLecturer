b03dab96eb9d54db9032ae878960633aee2539b3
JobGetabu <getabujob@gmail.com>
2018-11-16 14:35:57 +0300

    injected StepYStudy

--- app/src/main/java/com/job/darasalecturer/ui/newlesson/StepYStudyFragment.java
+++ app/src/main/java/com/job/darasalecturer/ui/newlesson/StepYStudyFragment.java
@@ -1,19 +1,34 @@
 package com.job.darasalecturer.ui.newlesson;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.job.darasalecturer.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StepYStudyFragment extends Fragment {
 
+
+    @BindView(R.id.step_ys_list)
+    RelativeLayout stepYsList;
+    @BindView(R.id.step_ys_back)
+    TextView stepYsBack;
+    @BindView(R.id.step_ys_next)
+    TextView stepYsNext;
+    Unbinder unbinder;
 
     public StepYStudyFragment() {
         // Required empty public constructor
@@ -24,7 +39,22 @@ public class StepYStudyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_ystudy, container, false);
+        View view = inflater.inflate(R.layout.fragment_step_ystudy, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.step_ys_back)
+    public void onStepYsBackClicked() {
+    }
+
+    @OnClick(R.id.step_ys_next)
+    public void onStepYsNextClicked() {
+    }
 }
