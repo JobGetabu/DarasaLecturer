9e21b8c433c4adb17bae1cc5fd6eb61dfc3f4a60
JobGetabu <getabujob@gmail.com>
2018-11-16 14:33:28 +0300

    course year adapter

--- app/src/main/java/com/job/darasalecturer/viewmodel/AddClassViewModel.java
+++ app/src/main/java/com/job/darasalecturer/viewmodel/AddClassViewModel.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.ViewModel;
 
+import com.job.darasalecturer.model.CourseYear;
 import com.job.darasalecturer.model.LecTeach;
 import com.job.darasalecturer.model.LecTeachTime;
 
@@ -15,15 +16,20 @@ import java.util.List;
 public class AddClassViewModel extends ViewModel {
 
     private MediatorLiveData<Integer> currentStep;
-    private MediatorLiveData<LecTeach> lecTeachMediatorLiveData = new MediatorLiveData<>();
-    private MediatorLiveData<LecTeachTime> lecTeachTimeMediatorLiveData = new MediatorLiveData<>();
-    private MediatorLiveData<List<String>> courseList = new MediatorLiveData<>();
+    private MediatorLiveData<LecTeach> lecTeachMediatorLiveData ;
+    private MediatorLiveData<LecTeachTime> lecTeachTimeMediatorLiveData;
+    private MediatorLiveData<List<String>> courseList ;
+    private MediatorLiveData<List<CourseYear>> courseYearList;
 
     private LiveData<LecTeach> lecTeachLiveData;
     private LiveData<LecTeachTime> lecTeachTimeLiveData;
 
     public AddClassViewModel() {
         currentStep = new MediatorLiveData<>();
+        lecTeachMediatorLiveData = new MediatorLiveData<>();
+        lecTeachTimeMediatorLiveData = new MediatorLiveData<>();
+        courseList = new MediatorLiveData<>();
+        courseYearList = new MediatorLiveData<>();
 
     }
 
@@ -57,5 +63,13 @@ public class AddClassViewModel extends ViewModel {
 
     public void setCourseList(List<String> courseList) {
         this.courseList.setValue(courseList);
+    }
+
+    public MediatorLiveData<List<CourseYear>> getCourseYearList() {
+        return courseYearList;
+    }
+
+    public void setCourseYearList(List<CourseYear> courseYearList) {
+        this.courseYearList.setValue(courseYearList);
     }
 }
