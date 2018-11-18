a632f2c514eb5fe6eeb8c105ae75817ceb808c38
JobGetabu <getabujob@gmail.com>
2018-11-18 16:17:13 +0300

    upated the listeners to click func

--- app/src/main/java/com/job/darasalecturer/util/CourseYearViewHolder.java
+++ app/src/main/java/com/job/darasalecturer/util/CourseYearViewHolder.java
@@ -1,5 +1,6 @@
 package com.job.darasalecturer.util;
 
+import android.support.design.card.MaterialCardView;
 import android.support.design.widget.TextInputLayout;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import com.leodroidcoder.genericadapter.OnRecyclerItemClickListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Job on Sunday : 11/18/2018.
@@ -21,10 +23,20 @@ public class CourseYearViewHolder extends BaseViewHolder<CourseYear, OnRecyclerI
     TextInputLayout textInputCourse;
     @BindView(R.id.sinc_yc_txt)
     TextView yrText;
+    @BindView(R.id.card_sinc_yc_id)
+    MaterialCardView cardView;
 
-    public CourseYearViewHolder(View itemView, OnRecyclerItemClickListener listener) {
+    OnRecyclerItemClickListener listener;
+
+    public CourseYearViewHolder(View itemView, final OnRecyclerItemClickListener listener) {
         super(itemView, listener);
         ButterKnife.bind(this,itemView);
+        this.listener = listener;
+    }
+
+    @OnClick({R.id.card_sinc_yc_id,R.id.sinc_yc_txt,R.id.sinc_yc_course})
+    public void onCardViewClicked(){
+        listener.onItemClick(getAdapterPosition());
     }
 
     @Override
