46b2d8b0d26249c5994ee0dbc8e15c7f584546d9
JobGetabu <getabujob@gmail.com>
2018-11-18 17:36:51 +0300

    added listener to course year adapters

--- app/src/main/java/com/job/darasalecturer/util/CourseYearViewHolder.java
+++ app/src/main/java/com/job/darasalecturer/util/CourseYearViewHolder.java
@@ -3,6 +3,7 @@ package com.job.darasalecturer.util;
 import android.support.design.card.MaterialCardView;
 import android.support.design.widget.TextInputLayout;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.job.darasalecturer.R;
@@ -25,18 +26,23 @@ public class CourseYearViewHolder extends BaseViewHolder<CourseYear, OnRecyclerI
     TextView yrText;
     @BindView(R.id.card_sinc_yc_id)
     MaterialCardView cardView;
+    @BindView(R.id.sinc_yc_drop)
+    ImageButton imageButton;
 
     OnRecyclerItemClickListener listener;
 
-    public CourseYearViewHolder(View itemView, final OnRecyclerItemClickListener listener) {
+
+    public CourseYearViewHolder(View itemView, final OnRecyclerItemClickListener listener,ImageClickListener imageClickListener) {
         super(itemView, listener);
         ButterKnife.bind(this,itemView);
         this.listener = listener;
+        this.imageClickListener = imageClickListener;
     }
 
-    @OnClick({R.id.card_sinc_yc_id,R.id.sinc_yc_txt,R.id.sinc_yc_course})
+    @OnClick({R.id.card_sinc_yc_id,R.id.sinc_yc_txt,R.id.sinc_yc_course,R.id.sinc_yc_drop})
     public void onCardViewClicked(){
-        listener.onItemClick(getAdapterPosition());
+        //listener.onItemClick(getAdapterPosition());
+        imageClickListener.onImageClick(getAdapterPosition(),listener,imageButton);
     }
 
     @Override
@@ -45,5 +51,11 @@ public class CourseYearViewHolder extends BaseViewHolder<CourseYear, OnRecyclerI
         textInputCourse.getEditText().setText(item.getCourse());
         yrText.setText("Year of Study: "+ item.getYearofstudyInt());
 
+    }
+
+    private ImageClickListener imageClickListener;
+
+    public interface ImageClickListener{
+        void onImageClick(int position, OnRecyclerItemClickListener listener, View itemView);
     }
 }
