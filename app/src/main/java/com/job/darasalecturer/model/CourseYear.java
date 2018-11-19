f82f9debe18f383a757314fe3b51e81295169b71
JobGetabu <getabujob@gmail.com>
2018-11-19 22:23:15 +0300

    added parcelable

--- app/src/main/java/com/job/darasalecturer/model/CourseYear.java
+++ app/src/main/java/com/job/darasalecturer/model/CourseYear.java
@@ -1,9 +1,12 @@
 package com.job.darasalecturer.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Job on Friday : 11/16/2018.
  */
-public class CourseYear  {
+public class CourseYear implements Parcelable {
     private String course;
     private int yearofstudy;
 
@@ -39,16 +42,32 @@ public class CourseYear  {
                 '}';
     }
 
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.course);
+        dest.writeInt(this.yearofstudy);
+    }
+
+    protected CourseYear(Parcel in) {
+        this.course = in.readString();
+        this.yearofstudy = in.readInt();
+    }
+
+    public static final Parcelable.Creator<CourseYear> CREATOR = new Parcelable.Creator<CourseYear>() {
+        @Override
+        public CourseYear createFromParcel(Parcel source) {
+            return new CourseYear(source);
+        }
+
+        @Override
+        public CourseYear[] newArray(int size) {
+            return new CourseYear[size];
+        }
+    };
 }
-
-
-/* List<Map<Integer, CourseYrData>> courseYrData = data.getCourseYrData();
-                        for (Map<Integer,CourseYrData> map : courseYrData){
-
-                            for (Map.Entry<Integer, CourseYrData> entery : map.entrySet()) {
-
-                                CourseYrData courseYrData1 = (CourseYrData) entery.getValue();
-                                Log.d(TAG, "onComplete: " + entery.getValue().toString());
-                                addCourses(courseYrData1.getCourseYear());
-                            }
-                        }*/
