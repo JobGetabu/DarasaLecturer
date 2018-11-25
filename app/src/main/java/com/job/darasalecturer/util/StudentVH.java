3f3bac2e3ac394c2196ad2ed71e6ed4fab19b51a
Job Getabu <getabujob@gmail.com>
2018-11-25 03:14:35 +0300

    added StudentVH

---
+++ app/src/main/java/com/job/darasalecturer/util/StudentVH.java
@@ -0,0 +1,86 @@
+package com.job.darasalecturer.util;
+
+import android.arch.lifecycle.LifecycleOwner;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hbb20.GThumb;
+import com.job.darasalecturer.R;
+import com.job.darasalecturer.model.StudentDetails;
+import com.job.darasalecturer.viewmodel.AddStudentViewModel;
+import com.leodroidcoder.genericadapter.BaseViewHolder;
+import com.leodroidcoder.genericadapter.OnRecyclerItemClickListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.refactor.library.SmoothCheckBox;
+
+/**
+ * Created by Job on Tuesday : 8/14/2018.
+ */
+public class StudentVH extends BaseViewHolder<StudentDetails, OnRecyclerItemClickListener> {
+
+    private static final String TAG = "StudentVH";
+    @BindView(R.id.attn_gthumb)
+    GThumb attnGthumb;
+    @BindView(R.id.attn_reg_no)
+    TextView attnRegNo;
+    @BindView(R.id.attn_stud_name)
+    TextView attnStudName;
+    @BindView(R.id.attn_check_box)
+    SmoothCheckBox attnCheckBox;
+    @BindView(R.id.attn_main)
+    ConstraintLayout attnMain;
+
+    private LifecycleOwner mActivity;
+    private FirebaseFirestore mFirestore;
+    private StudentDetails model;
+    private AddStudentViewModel addStudentViewModel;
+
+    public StudentVH(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+
+        //LayoutInflater.from(mContext).inflate(R.layout.single_attendance, null);
+
+        attnCheckBox.setOnCheckedChangeListener(new SmoothCheckBox.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SmoothCheckBox checkBox, boolean isChecked) {
+
+                if (isChecked) {
+
+                    addStudentViewModel.getStudentDetailsList().add(model);
+                    addStudentViewModel.setStudListMediatorLiveData(addStudentViewModel.getStudentDetailsList());
+                } else {
+
+                    addStudentViewModel.getStudentDetailsList().remove(model);
+                    addStudentViewModel.setStudListMediatorLiveData(addStudentViewModel.getStudentDetailsList());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBind(StudentDetails item) {
+        setUpUi(item);
+    }
+
+    public void init(LifecycleOwner mActivity, FirebaseFirestore mFirestore,
+                     StudentDetails model, AddStudentViewModel addStudentViewModel) {
+        this.mActivity = mActivity;
+        this.mFirestore = mFirestore;
+        this.model = model;
+        this.addStudentViewModel = addStudentViewModel;
+    }
+
+    public void setUpUi(StudentDetails model) {
+
+        attnStudName.setText(model.getFirstname() + " " + model.getLastname());
+        attnRegNo.setText(model.getRegnumber());
+        attnGthumb.loadThumbForName(model.getPhotourl(), model.getFirstname(), model.getLastname());
+
+    }
+}
