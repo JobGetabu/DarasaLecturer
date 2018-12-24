38555068480d542c357597f472201d417a918d35
Job Getabu <getabujob@gmail.com>
2018-12-24 14:09:14 +0300

    added ClassListModel

---
+++ app/src/main/java/com/job/darasalecturer/model/ClassListModel.java
@@ -0,0 +1,53 @@
+package com.job.darasalecturer.model;
+
+/**
+ * Created by Job on Monday : 12/24/2018.
+ */
+public class ClassListModel {
+
+    private String name;
+    private String regno;
+    private String email;
+
+    public ClassListModel() {
+    }
+
+    public ClassListModel(String name, String regno, String email) {
+        this.name = name;
+        this.regno = regno;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegno() {
+        return regno;
+    }
+
+    public void setRegno(String regno) {
+        this.regno = regno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassListModel{" +
+                "name='" + name + '\'' +
+                ", regno='" + regno + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
