b3932ce0242076f810f5d800a6f3da258348cd43
Job Getabu <getabujob@gmail.com>
2018-12-31 14:18:28 +0300

    added model IconPowerMenuCourse

---
+++ app/src/main/java/com/job/darasalecturer/util/IconPowerMenuCourse.java
@@ -0,0 +1,30 @@
+package com.job.darasalecturer.util;
+
+/**
+ * Created by Job on Monday : 12/31/2018.
+ */
+public class IconPowerMenuCourse {
+    private String key;
+    private String title;
+
+    public IconPowerMenuCourse(String key, String title) {
+        this.key = key;
+        this.title = title;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
