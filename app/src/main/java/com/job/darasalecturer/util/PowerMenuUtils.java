f970e90e987450c3e101105227a364685ff4fc71
Job Getabu <getabujob@gmail.com>
2018-12-31 17:23:06 +0300

    added power menu utils

---
+++ app/src/main/java/com/job/darasalecturer/util/PowerMenuUtils.java
@@ -0,0 +1,68 @@
+package com.job.darasalecturer.util;
+
+import android.arch.lifecycle.LifecycleOwner;
+import android.content.Context;
+
+import com.google.firebase.Timestamp;
+import com.job.darasalecturer.adapter.IconMenuAdapterCourses;
+import com.job.darasalecturer.adapter.IconMenuAdapterDates;
+import com.job.darasalecturer.model.CourseYear;
+import com.job.darasalecturer.model.IconPowerMenuCourse;
+import com.job.darasalecturer.model.IconPowerMenuDate;
+import com.job.darasalecturer.model.SavedClasses;
+import com.skydoves.powermenu.CustomPowerMenu;
+import com.skydoves.powermenu.MenuAnimation;
+import com.skydoves.powermenu.OnMenuItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Job on Monday : 12/31/2018.
+ */
+public class PowerMenuUtils {
+
+    public static CustomPowerMenu getCoursesCustomDialogPowerMenu(Context context, LifecycleOwner lifecycleOwner,List<CourseYear> courseYears,
+                                                           OnMenuItemClickListener<IconPowerMenuCourse> onIconCourseMenuItemClickListener) {
+
+        List<IconPowerMenuCourse> iconPowerMenuCourses = new ArrayList<>();
+        for (CourseYear s : courseYears) {
+            IconPowerMenuCourse ipmd = new IconPowerMenuCourse(String.valueOf(s.getYearofstudy()),s.getCourse());
+            iconPowerMenuCourses.add(ipmd);
+        }
+
+        return new CustomPowerMenu.Builder<>(context,
+                new IconMenuAdapterCourses())
+                .addItemList(iconPowerMenuCourses)
+                .setOnMenuItemClickListener(onIconCourseMenuItemClickListener)
+                .setAnimation(MenuAnimation.SHOWUP_BOTTOM_RIGHT)
+                .setLifecycleOwner(lifecycleOwner)
+                .setMenuRadius(10f)
+                .setMenuShadow(10f)
+                .build();
+    }
+
+    public static CustomPowerMenu getDatesCustomDialogPowerMenu(Context context, LifecycleOwner lifecycleOwner,List<Timestamp> timestamps,
+                                                                  OnMenuItemClickListener<IconPowerMenuCourse> onIconCourseMenuItemClickListener) {
+
+        List<IconPowerMenuDate> iconPowerMenudates = new ArrayList<>();
+
+        for (Timestamp ts : timestamps) {
+            String key = SavedClasses.formatDateKey(ts.toDate());
+            String title = SavedClasses.formatDate(ts.toDate());
+
+            IconPowerMenuDate ipmd = new IconPowerMenuDate(key,title);
+            iconPowerMenudates.add(ipmd);
+        }
+
+        return new CustomPowerMenu.Builder<>(context,
+                new IconMenuAdapterDates())
+                .addItemList(iconPowerMenudates)
+                .setOnMenuItemClickListener(onIconCourseMenuItemClickListener)
+                .setAnimation(MenuAnimation.SHOWUP_BOTTOM_RIGHT)
+                .setLifecycleOwner(lifecycleOwner)
+                .setMenuRadius(10f)
+                .setMenuShadow(10f)
+                .build();
+    }
+}
